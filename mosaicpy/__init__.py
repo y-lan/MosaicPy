fae480ce5e87fe64e7eb55ee92c81a97596fd00b
lan <lanyuyang@gmail.com>
2023-11-27 11:57:32 +0900

    add short cut mpy.lists

--- mosaicpy/__init__.py
+++ mosaicpy/__init__.py
@@ -1,6 +1,6 @@
 from . import notebook, utils, llm, collections
 
-from .collections import dict, groupby, pmap, sample
+from .collections import dict, groupby, pmap, sample, lists
 from .llm.openai.chat import OpenAIBot
 from .utils.file import load_jsonl, dump_jsonl, load_pickle, dump_pickle
 from .utils.time import time_it
