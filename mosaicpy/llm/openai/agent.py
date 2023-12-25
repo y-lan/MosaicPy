aae01df3371584a96fdc26218a9d72b6e5aa1336
lan <lanyuyang@gmail.com>
2023-12-26 00:32:56 +0900

    add support of azure openai

--- mosaicpy/llm/openai/agent.py
+++ mosaicpy/llm/openai/agent.py
@@ -5,6 +5,7 @@ import os
 import time
 import openai
 import urllib
+from openai import AzureOpenAI
 from openai.types import CompletionUsage
 
 from pydantic import BaseModel
@@ -24,29 +25,23 @@ logger = logging.getLogger(__name__)
 def _create_image_content(image_path):
     parsed = urllib.parse.urlparse(image_path)
 
-    if parsed.scheme in ('http', 'https'):
+    if parsed.scheme in ("http", "https"):
         return {
             "type": "image_url",
-            "image_url": {
-                "url": image_path
-            },
+            "image_url": {"url": image_path},
         }
     elif os.path.exists(image_path):
         with open(image_path, "rb") as image_file:
-            base64_image = base64.b64encode(image_file.read()).decode('utf-8')
+            base64_image = base64.b64encode(image_file.read()).decode("utf-8")
 
             return {
                 "type": "image_url",
-                "image_url": {
-                    "url": f"data:image/jpeg;base64,{base64_image}"
-                },
+                "image_url": {"url": f"data:image/jpeg;base64,{base64_image}"},
             }
     elif image_path.startswith("data:image/jpeg;base64,"):
         return {
             "type": "image_url",
-            "image_url": {
-                "url": image_path
-            },
+            "image_url": {"url": image_path},
         }
     else:
         raise Exception(f"Invalid image path: {image_path}")
@@ -58,8 +53,8 @@ class TokenUsage(BaseModel):
 
 
 class AgentConfig(BaseModel):
-    model_name: str = 'gpt-3.5-turbo-1106'
-    system_prompt: str = 'You are a helpful assistant'
+    model_name: str = "gpt-3.5-turbo-1106"
+    system_prompt: str = "You are a helpful assistant"
     temperature: float = 0.1
     keep_conversation_state: bool = False
     max_retry: int = 16
@@ -73,11 +68,13 @@ class AgentConfig(BaseModel):
     frequency_penalty: float = 0
     top_p: float = 1
     seed: int = None
+    use_azure: bool = False
+    azure_endpoint: str = None
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
 
-        if self.model_name in ['gpt-4-vision-preview']:
+        if self.model_name in ["gpt-4-vision-preview"]:
             self.support_tools = False
 
     class Config:
@@ -86,7 +83,6 @@ class AgentConfig(BaseModel):
 
 class OpenAIAgent:
     def __init__(self, config: AgentConfig = None, tools: list[Tool] = None, **kwargs):
-
         if config is None:
             config = AgentConfig(**kwargs)
         self.config = config
@@ -108,34 +104,44 @@ class OpenAIAgent:
             event = Event[event.upper()]
 
         if event == Event.NEW_CHAT_TOKEN and not self.config.stream:
-            logger.warning(
-                "Event.NEW_CHAT_TOKEN is only available when stream=True")
+            logger.warning("Event.NEW_CHAT_TOKEN is only available when stream=True")
 
-        assert isinstance(
-            event, Event), "event must be an instance of Event enum"
+        assert isinstance(event, Event), "event must be an instance of Event enum"
         self.event_manager.subscribe(event, callback)
+
+    def _get_client(self):
+        if self.config.use_azure:
+            assert (
+                os.environ["AZURE_OPENAI_API_KEY"] is not None
+            ), "AZURE_OPENAI_API_KEY must be provided"
+            assert self.config.azure_endpoint is not None, "Azure endpoint must be provided"
+            return AzureOpenAI(
+                api_version="2023-07-01-preview",
+                azure_endpoint=self.config.azure_endpoint,
+            )
+        else:
+            return openai
 
     def add_ai_history(self, history_message):
         self.conversation_state.append(
-            mdict(role='assistant', content=[
-                  mdict(type='text', text=history_message)])
+            mdict(role="assistant", content=[mdict(type="text", text=history_message)])
         )
 
     def add_user_history(self, history_message):
         self.conversation_state.append(
-            mdict(role='user', content=[
-                  mdict(type='text', text=history_message)])
+            mdict(role="user", content=[mdict(type="text", text=history_message)])
         )
 
     def _get_system_msg(self):
         if self.config.enable_magic_placeholders:
-            return {"role": "system", "content": replace_magic_placeholders(self.config.system_prompt)}
+            return {
+                "role": "system",
+                "content": replace_magic_placeholders(self.config.system_prompt),
+            }
         else:
             return {"role": "system", "content": self.config.system_prompt}
 
-    def _call_completion(self,
-                         msgs, max_tokens, generate_n, temperature,
-                         tools=None):
+    def _call_completion(self, msgs, max_tokens, generate_n, temperature, tools=None):
         kwargs = {
             "model": self.config.model_name,
             "messages": msgs,
@@ -153,22 +159,22 @@ class OpenAIAgent:
             kwargs["tool_choice"] = "auto"
 
         if self.config.stream:
-            kwargs['stream'] = True
+            kwargs["stream"] = True
 
         if self.config.json_output:
-            kwargs['response_format'] = {"type": "json_object"}
+            kwargs["response_format"] = {"type": "json_object"}
 
-        logger.debug(f'Request to OpenAI: {kwargs}')
+        logger.debug(f"Request to OpenAI: {kwargs}")
 
         for _ in range(self.config.max_retry):
             try:
-                completion = openai.chat.completions.create(**kwargs)
+                client = self._get_client()
+                completion = client.chat.completions.create(**kwargs)
                 break
             except openai.RateLimitError:
                 if self.config.verbose:
-                    logger.warning(
-                        f"Rate limit exceeded. Retrying in {2 ** _} seconds...")
-                time.sleep(2 ** _)
+                    logger.warning(f"Rate limit exceeded. Retrying in {2 ** _} seconds...")
+                time.sleep(2**_)
             except openai.APITimeoutError:
                 pass
         else:
@@ -186,38 +192,34 @@ class OpenAIAgent:
         estimated_usage_completion = estimate_response_tokens(completion)
 
         logger.debug(
-            f'Estimated token usage: prompt={estimated_usage_prompt}, completion={estimated_usage_completion}')
+            f"Estimated token usage: prompt={estimated_usage_prompt}, completion={estimated_usage_completion}"
+        )
 
         if completion.usage:
             logger.debug(
-                f'Actual token usage: prompt={completion.usage.prompt_tokens}, completion={completion.usage.completion_tokens}')
+                f"Actual token usage: prompt={completion.usage.prompt_tokens}, completion={completion.usage.completion_tokens}"
+            )
         else:
             completion.usage = CompletionUsage(
                 completion_tokens=estimated_usage_completion,
                 prompt_tokens=estimated_usage_prompt,
-                total_tokens=estimated_usage_prompt + estimated_usage_completion
+                total_tokens=estimated_usage_prompt + estimated_usage_completion,
             )
 
-        logger.debug(f'Response from OpenAI: {completion}')
+        logger.debug(f"Response from OpenAI: {completion}")
 
         return completion
 
-    def chat(self,
-             user_input,
-             image=None,
-             temperature=None,
-             max_tokens=1024,
-             generate_n=1,
-             **kwargs):
-
+    def chat(
+        self, user_input, image=None, temperature=None, max_tokens=1024, generate_n=1, **kwargs
+    ):
         # if kwargs is not None, loop it to format the user_input
         if kwargs:
             user_input = user_input.format(**kwargs)
 
-        user_msg = {"role": "user", "content": [
-            mdict(type='text', text=user_input)]}
+        user_msg = {"role": "user", "content": [mdict(type="text", text=user_input)]}
         if image is not None:
-            user_msg['content'].append(_create_image_content(image))
+            user_msg["content"].append(_create_image_content(image))
 
         msgs = [self._get_system_msg()]
         if self.config.keep_conversation_state:
@@ -225,11 +227,9 @@ class OpenAIAgent:
             self.conversation_state.append(user_msg)
         msgs.append(user_msg)
 
-        tools = [build_function_signature(tool)
-                 for tool in self.tools.values()]
+        tools = [build_function_signature(tool) for tool in self.tools.values()]
 
-        completion = self._call_completion(
-            msgs, max_tokens, generate_n, temperature, tools=tools)
+        completion = self._call_completion(msgs, max_tokens, generate_n, temperature, tools=tools)
 
         if completion.choices[0].message.tool_calls:
             msgs.append(completion.choices[0].message)
@@ -240,29 +240,28 @@ class OpenAIAgent:
                     function_args = json.loads(tool_call.function.arguments)
 
                     res = self.tools[function_name]._run(**function_args)
-                    logger.info(
-                        f'Tool call: {function_name}({function_args}) ->\n###\n{res}\n###')
+                    logger.info(f"Tool call: {function_name}({function_args}) ->\n###\n{res}\n###")
 
-                    msgs.append(mdict(
-                        tool_call_id=tool_call.id,
-                        role='tool',
-                        name=function_name,
-                        content=f'{res}'))
+                    msgs.append(
+                        mdict(
+                            tool_call_id=tool_call.id,
+                            role="tool",
+                            name=function_name,
+                            content=f"{res}",
+                        )
+                    )
             else:
                 res = []
                 for tool_call in completion.choices[0].message.tool_calls:
                     function_name = tool_call.function.name
                     function_args = json.loads(tool_call.function.arguments)
 
-                    res.append(
-                        f'{function_name}({function_args})'
-                    )
+                    res.append(f"{function_name}({function_args})")
 
-                print('\n'.join(res))
-                return ''
+                print("\n".join(res))
+                return ""
 
-            completion = self._call_completion(
-                msgs, max_tokens, generate_n, temperature)
+            completion = self._call_completion(msgs, max_tokens, generate_n, temperature)
 
         res = completion.choices
 
@@ -273,12 +272,14 @@ class OpenAIAgent:
 
         if self.config.keep_conversation_state:
             self.conversation_state.append(
-                mdict(role='assistant', content=[mdict(type='text', text=res)])
+                mdict(role="assistant", content=[mdict(type="text", text=res)])
             )
 
-        self.event_manager.publish(Event.FINISH_CHAT,
-                                   prompt_tokens=completion.usage.prompt_tokens,
-                                   completion_tokens=completion.usage.completion_tokens,
-                                   response=res,)
+        self.event_manager.publish(
+            Event.FINISH_CHAT,
+            prompt_tokens=completion.usage.prompt_tokens,
+            completion_tokens=completion.usage.completion_tokens,
+            response=res,
+        )
 
         return res
